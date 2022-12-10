b87dbf1c4773e280a3fa677fd8e3ffac3fcb0325
Eric Brombaugh <ebrombaugh1@cox.net>
2022-12-10 11:12:32 -0700

    Added 52kB SPRAM and rearranged I/O to support BASIC

--- Gateware/muacm_example/rtl/cpu_system.v
+++ Gateware/muacm_example/rtl/cpu_system.v
@@ -40,35 +40,45 @@ module cpu_system(
     );
     
 	// address decode - not fully decoded for 512-byte memories
-	wire p0 = (CPU_AB[15:12] == 4'h0) ? 1 : 0;
-	wire p1 = (CPU_AB[15:12] == 4'h1) ? 1 : 0;
-	wire p2 = (CPU_AB[15:12] == 4'h2) ? 1 : 0;
-	wire pf = (CPU_AB[15:12] == 4'hf) ? 1 : 0;
+	wire sel_ram0 = (CPU_AB[15] == 1'b0) ? 1 : 0;
+	wire sel_ram1 = ((CPU_AB[15:12] >= 4'h8)&&(CPU_AB[15:12] <= 4'hC)) ? 1 : 0;
+	wire sel_acia = (CPU_AB[15:8] == 8'hf0) ? 1 : 0;
+	wire sel_gpio = (CPU_AB[15:8] == 8'hf2) ? 1 : 0;
+	wire sel_rom  = (CPU_AB[15:11] == 5'h1f) ? 1 : 0;
 	
-	// RAM @ pages 00-0f
-	reg [7:0] ram_mem[4095:0];
-	reg [7:0] ram_do;
-	always @(posedge clk)
-		if((CPU_WE == 1'b1) && (p0 == 1'b1))
-			ram_mem[CPU_AB[11:0]] <= CPU_DO;
-	always @(posedge clk)
-		ram_do <= ram_mem[CPU_AB[11:0]];
+	// RAM write protects
+	reg [7:0] ram0_wp, ram1_wp;
 	
-	// GPIO @ page 10-1f
-	reg [7:0] gpio_do;
-	always @(posedge clk)
-		if((CPU_WE == 1'b1) && (p1 == 1'b1))
-			gpio_o <= CPU_DO;
+	// 32kB RAM @ 0000-7FFF
+	wire [7:0] ram0_do;
+	ram_32kb uram0(
+		.clk(clk),
+		.sel(sel_ram0),
+		.we(CPU_WE),
+		.wp(ram0_wp),
+		.addr(CPU_AB[14:0]),
+		.din(CPU_DO),
+		.dout(ram0_do)
+	);
 	
-	always @(posedge clk)
-		gpio_do <= gpio_i;
+	// 20kB RAM @ 8000-CFFF
+	wire [7:0] ram1_do;
+	ram_32kb uram1(
+		.clk(clk),
+		.sel(sel_ram1),
+		.we(CPU_WE),
+		.wp(ram1_wp),
+		.addr(CPU_AB[14:0]),
+		.din(CPU_DO),
+		.dout(ram1_do)
+	);
 	
-	// ACIA at page 20-2f
+	// ACIA @ F000-F0FF
 	wire [7:0] acia_do;
 	usb_acia uacia(
 		.clk(clk),				// system clock
 		.rst(rst),				// system reset
-		.cs(p2),				// chip select
+		.cs(sel_acia),			// chip select
 		.we(CPU_WE),			// write enable
 		.rs(CPU_AB[0]),			// register select
 		.din(CPU_DO),			// data bus input
@@ -82,24 +92,55 @@ module cpu_system(
 		.irq(CPU_IRQ)			// interrupt request
 	);
 	
-	// ROM @ pages f0,f1...
-    reg [7:0] rom_mem[4095:0];
+	// 256B GPIO & write protects @ F200-F2FF
+	reg [7:0] gpio_do;
+	// write
+	always @(posedge clk)
+		if(rst)
+		begin
+			gpio_o <= 8'h00;
+			ram0_wp <= 8'h00;
+			ram1_wp <= 8'h00;
+		end
+		else if(CPU_WE & sel_gpio)
+		begin
+			case(CPU_AB[1:0])
+				2'b00: gpio_o <= CPU_DO;
+				2'b10: ram0_wp <= CPU_DO;
+				2'b11: ram1_wp <= CPU_DO;
+			endcase
+		end
+		
+	// read
+	always @(posedge clk)
+		if(!CPU_WE & sel_gpio)
+			case(CPU_AB[1:0])
+				2'b00: gpio_do <= gpio_o;
+				2'b01: gpio_do <= gpio_i;
+				2'b10: gpio_do <= ram0_wp;
+				2'b11: gpio_do <= ram1_wp;
+			endcase
+			
+	// 2kB ROM @ F800-FFFF
+    reg [7:0] rom_mem[2047:0];
 	reg [7:0] rom_do;
 	initial
-        $readmemh("rtl/rom.hex",rom_mem);
+        $readmemh("rom.hex",rom_mem);
 	always @(posedge clk)
-		rom_do <= rom_mem[CPU_AB[11:0]];
+		rom_do <= rom_mem[CPU_AB[10:0]];
 
 	// data mux
 	reg [3:0] mux_sel;
 	always @(posedge clk)
-		mux_sel <= CPU_AB[15:12];
+		mux_sel <= {sel_rom,sel_gpio,sel_acia,sel_ram1,sel_ram0};
 	always @(*)
 		casez(mux_sel)
-			4'h0: CPU_DI = ram_do;
-			4'h1: CPU_DI = gpio_do;
-			4'h2: CPU_DI = acia_do;
-			4'hf: CPU_DI = rom_do;
+			5'b00001: CPU_DI = ram0_do;
+			5'b00010: CPU_DI = ram1_do;
+			5'b00100: CPU_DI = acia_do;
+			5'b01000: CPU_DI = gpio_do;
+			5'b10000: CPU_DI = rom_do;
 			default: CPU_DI = rom_do;
-		endcase`endif
+		endcase
+`endif
 endmodule
